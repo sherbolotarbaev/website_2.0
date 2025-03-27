78474b658e9ccdec41a715fbb914da4c54a9bda3
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2025-03-28 00:41:04 +0600

    update: action

--- src/lib/actions/contact.ts
+++ src/lib/actions/contact.ts
@@ -6,11 +6,12 @@ import { headers as requestHeaders } from 'next/headers'
 import { userAgent as getUserAgent } from 'next/server'
 import type { z } from 'zod'
 
-const BOT_TOKEN = process.env.TELEGRAM_BOT_API_KEY
-const CHAT_ID = process.env.TELEGRAM_BOT_CHAT_ID
-
 async function getLocation(ip: string) {
-	const res = await fetch(`https://ipwho.is/${ip}`)
+	const res = await fetch(`https://ipwho.is/${ip}`, {
+		headers: {
+			origin: 'https://ipwho.is',
+		},
+	})
 	return res.json()
 }
 
@@ -20,6 +21,16 @@ const escapeHTML = (str: string) =>
 export async function newMessage(
 	formData: z.infer<typeof ContactFormSchema>
 ): Promise<{ status: 'success' | 'error'; reason?: string }> {
+	const BOT_TOKEN = process.env.TELEGRAM_BOT_API_KEY
+	const CHAT_ID = process.env.TELEGRAM_BOT_CHAT_ID
+
+	if (!BOT_TOKEN || !CHAT_ID) {
+		return {
+			status: 'error',
+			reason: 'Invalid Telegram Bot Token or Chat ID.',
+		}
+	}
+
 	const headers = await requestHeaders()
 	const ip =
 		headers.get('x-forwarded-for') ||
@@ -40,7 +51,10 @@ export async function newMessage(
 		!location.region ||
 		!location.country
 	) {
-		return { status: 'error', reason: 'Failed to get location' }
+		return {
+			status: 'error',
+			reason: 'Failed to get location.',
+		}
 	}
 
 	const time = formatInTimeZone(
@@ -53,7 +67,10 @@ export async function newMessage(
 		const validatedFields = ContactFormSchema.safeParse(formData)
 
 		if (!validatedFields.success) {
-			return { status: 'error', reason: 'Invalid form data' }
+			return {
+				status: 'error',
+				reason: 'Invalid form data.',
+			}
 		}
 
 		const message = `
@@ -93,6 +110,15 @@ export async function newMessage(
 
 		return { status: 'success' }
 	} catch (error: any) {
-		return { status: 'error', reason: error.message }
+		return {
+			status: 'error',
+			reason: `Error: ${error.message} Location: ${location.city}, ${
+				location.region
+			}, ${location.country} IP: ${ip} Telegram Bot Token: ${
+				BOT_TOKEN.substring(0, 6) + '*'.repeat(BOT_TOKEN.length - 6)
+			} Telegram Chat ID: ${
+				CHAT_ID?.substring(0, 3) + '*'.repeat(CHAT_ID?.length - 6)
+			}`,
+		}
 	}
 }
